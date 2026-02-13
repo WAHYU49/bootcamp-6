d7b1163a13ad090479fd55d26f4760738f109e9a
WAHYU49 <wahyucahyovebrian@gmail.com>
2026-02-13 20:02:11 +0700

    update

--- ecommerce-b6/app/Http/Controllers/HomeController.php
+++ ecommerce-b6/app/Http/Controllers/HomeController.php
@@ -11,19 +11,38 @@ class HomeController extends Controller
         $name = "<b>Wahyu C</b>";
 
         $products = [
-            'Product 1',
-            'Product 2',
-            'Product 3',
-            'Product 4',
+           [
+                'id' => 1,
+                'name' => 'Product 1',
+                'description' => 'This is the description for Product 1',
+                'price' => 100000,
+                'image' => '/images/products/Mengapa-Website-Dianggap-Sebagai-Alat-Bisnis-yang-Sangat-Powerful.jpg',
+           ],
+           [
+                'id' => 2,
+                'name' => 'Product 2',
+                'description' => 'This is the description for Product 2',
+                'price' => 150000,
+                'image' => '/images/products/product2.jpg',
+           ],
+           [
+                'id' => 3,
+                'name' => 'Product 3',
+                'description' => 'This is the description for Product 3',
+                'price' => 200000,
+                'image' => '/images/products/product3.jpg',
+           ]   
         ];
 
-        return view('home');
+        $is_logged_in = false;
+
+        return view('home', compact('name', 'products', 'is_logged_in'));
 
      
     }
 
     public function productDetail($id)
     {
-        return "Product Detail for Product ID: " . $id;
+        return view('product-detail', compact('id'));
     }
 }
