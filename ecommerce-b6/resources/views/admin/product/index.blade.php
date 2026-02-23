3978dd078f0689c02959c804a4c31b91c41bd27b
WAHYU49 <wahyucahyovebrian@gmail.com>
2026-02-23 18:42:55 +0700

    update

--- ecommerce-b6/resources/views/admin/product/index.blade.php
+++ ecommerce-b6/resources/views/admin/product/index.blade.php
@@ -14,7 +14,7 @@
                         <a href="{{ route('products.create') }}" 
                         class="bg-blue-500 hover:bg-blue-700 text-white font-bold 
                         py-2 px-4 rounded">
-                           + Tambah Kategori
+                           + Tambah Produk
                         </a>
                     </div>
                     <table class="w-full bg-white border" id="myTable">
